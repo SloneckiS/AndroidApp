add2ad8db58fd74dba3408ef90f89a08e99e976d
szymonslonecki <szymon.slonecki@o2.pl>
2021-03-06 00:06:30 +0100

    clean basket after order

--- app/src/main/java/com/example/KitShop/PaymentDetails.java
+++ app/src/main/java/com/example/KitShop/PaymentDetails.java
@@ -9,6 +9,9 @@ import android.widget.TextView;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PaymentDetails extends AppCompatActivity {
 
     TextView txtId,txtAmount,txtStatus;
@@ -18,7 +21,9 @@ public class PaymentDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_details);
-
+        Basket.products = new ArrayList<>();
+        Basket.price = new ArrayList<>();
+        Basket.totalPrice = 0.0;
         txtId = (TextView)findViewById(R.id.txtId);
         txtAmount = (TextView)findViewById(R.id.txtAmount);
         txtStatus = (TextView)findViewById(R.id.txtStatus);

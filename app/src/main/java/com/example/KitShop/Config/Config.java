fe6d4a1e2a84c033e5c15db1a468cac5749ae189
szymonslonecki <szymon.slonecki@o2.pl>
2021-03-07 16:48:11 +0100

    update paypal

--- app/src/main/java/com/example/KitShop/Config/Config.java
+++ app/src/main/java/com/example/KitShop/Config/Config.java
@@ -1,5 +1,5 @@
 package com.example.KitShop.Config;
 
 public class Config {
-    public static final String PAYPAL_CLIENT_ID = "ASjongJxQzhts3caXHzxe6-2yLiyBRB4AOt_eDyKPhU7_gQOskxybjw7Pg5glIoVz5pFAbpWWFlbdN2y";
+    public static final String PAYPAL_CLIENT_ID = "AbEqBUMM6pC8ClFo4bGjDyj431rD4C9lypYhuxMo7ghWF70njj7kiNVekGK2sKE5cWgfENOu04MGME6E";
 }
